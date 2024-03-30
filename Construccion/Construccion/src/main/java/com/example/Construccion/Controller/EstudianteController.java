b7e224e71fabbad784ac9d03ea0255390099eade
LAPTOP-RPL1IHFP\allib <adlopez45@ucatolica.edu.co>
2024-03-29 19:48:52 -0500

    Se incluyen modificaciones

---
+++ Construccion/Construccion/src/main/java/com/example/Construccion/Controller/EstudianteController.java
@@ -0,0 +1,59 @@
+package com.example.Construccion.Controller;
+
+import com.example.Construccion.entity.Estudiante;
+import com.example.Construccion.service.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/estudiantes")
+public class EstudianteController {
+
+    private final EstudianteService estudianteService;
+
+    @Autowired
+    public EstudianteController(EstudianteService estudianteService) {
+        this.estudianteService = estudianteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Estudiante>> getAllEstudiantes() {
+        List<Estudiante> estudiantes = estudianteService.getAllEstudiantes();
+        return new ResponseEntity<>(estudiantes, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estudiante> getEstudianteById(@PathVariable("id") int id) {
+        Estudiante estudiante = estudianteService.getEstudianteById(id);
+        if (estudiante == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(estudiante, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Estudiante> createEstudiante(@RequestBody Estudiante estudiante) {
+        Estudiante createdEstudiante = estudianteService.createEstudiante(estudiante);
+        return new ResponseEntity<>(createdEstudiante, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estudiante> updateEstudiante(@PathVariable("id") int id, @RequestBody Estudiante estudiante) {
+        Estudiante updatedEstudiante = estudianteService.updateEstudiante(id, estudiante);
+        if (updatedEstudiante == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(updatedEstudiante, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEstudiante(@PathVariable("id") int id) {
+        estudianteService.deleteEstudiante(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
+
